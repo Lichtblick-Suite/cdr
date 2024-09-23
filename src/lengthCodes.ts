f5a96066fa170036a897f4b773616f0881174300
João Miguel Luis <CTW03844@criticaltechworks.com>
2024-09-23 13:48:30 +0100

    fixing lint unwanted change

--- src/lengthCodes.ts
+++ src/lengthCodes.ts
@@ -1,4 +1,11 @@
 export type LengthCode = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7;
+export const lengthCodeToObjectSizes = {
+  0: 1,
+  1: 2,
+  2: 4,
+  3: 8,
+};
+const maxSize = 0xffffffff;
 
 export function getLengthCodeForObjectSize(objectSize: number): LengthCode {
   let defaultLengthCode: LengthCode | undefined;
@@ -20,7 +27,6 @@ export function getLengthCodeForObjectSize(objectSize: number): LengthCode {
 
   if (defaultLengthCode == undefined) {
     // Not currently supporting writing of lengthCodes > 4
-    const maxSize = 0xffffffff;
     if (objectSize > maxSize) {
       throw Error(
         `Object size ${objectSize} for EMHEADER too large without specifying length code. Max size is ${maxSize}`,
@@ -30,10 +36,3 @@ export function getLengthCodeForObjectSize(objectSize: number): LengthCode {
   }
   return defaultLengthCode;
 }
-
-export const lengthCodeToObjectSizes = {
-  0: 1,
-  1: 2,
-  2: 4,
-  3: 8,
-};
